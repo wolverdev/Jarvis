3ee09a9b02c5165dc4b5e5ed6ad5cb7f14f2e01c
math15 <matheusifrj@hotmail.com>
2025-05-24 10:52:28 +0700

    update Jarvis cli and stub

--- resources/views/cli/jarvis.blade.php
+++ resources/views/cli/jarvis.blade.php
@@ -6,6 +6,7 @@
     'storeRequest',
     'updateRequest',
     'pageIndex',
+    'pageForm',
     'pageCreate',
     'pageDelete',
     'pageDeleteBulk',
@@ -13,65 +14,123 @@
     'permission',
     'route'
 ])
+
 <div class="mx-2 my-1">
     <div class="my-1">
         <span class="font-bold text-green">Jarvis Resource Generated</span>
 
-        <div class="flex space-x-1">
-            <span class="font-bold">{{ $controller }}</span>
-            <span class="flex-1 content-repeat-[.] text-gray"></span>
-            <span class="font-bold text-green uppercase">created</span>
-        </div>
-        <div class="flex space-x-1">
-            <span class="font-bold">{{ $model }}</span>
-            <span class="flex-1 content-repeat-[.] text-gray"></span>
-            <span class="font-bold text-green uppercase">created</span>
-        </div>
-        <div class="flex space-x-1">
-            <span class="font-bold">{{ $migration }}</span>
-            <span class="flex-1 content-repeat-[.] text-gray"></span>
-            <span class="font-bold text-green uppercase">created</span>
-        </div>
-        <div class="flex space-x-1">
-            <span class="font-bold">{{ $indexRequest }}</span>
-            <span class="flex-1 content-repeat-[.] text-gray"></span>
-            <span class="font-bold text-green uppercase">created</span>
-        </div>
-        <div class="flex space-x-1">
-            <span class="font-bold">{{ $storeRequest }}</span>
-            <span class="flex-1 content-repeat-[.] text-gray"></span>
-            <span class="font-bold text-green uppercase">created</span>
-        </div>
-        <div class="flex space-x-1">
-            <span class="font-bold">{{ $updateRequest }}</span>
-            <span class="flex-1 content-repeat-[.] text-gray"></span>
-            <span class="font-bold text-green uppercase">created</span>
-        </div>
-        <div class="flex space-x-1">
-            <span class="font-bold">{{ $pageIndex }}</span>
-            <span class="flex-1 content-repeat-[.] text-gray"></span>
-            <span class="font-bold text-green uppercase">created</span>
-        </div>
-        <div class="flex space-x-1">
-            <span class="font-bold">{{ $pageCreate }}</span>
-            <span class="flex-1 content-repeat-[.] text-gray"></span>
-            <span class="font-bold text-green uppercase">created</span>
-        </div>
-        <div class="flex space-x-1">
-            <span class="font-bold">{{ $pageEdit }}</span>
-            <span class="flex-1 content-repeat-[.] text-gray"></span>
-            <span class="font-bold text-green uppercase">created</span>
-        </div>
-        <div class="flex space-x-1">
-            <span class="font-bold">{{ $permission }}</span>
-            <span class="flex-1 content-repeat-[.] text-gray"></span>
-            <span class="font-bold text-green uppercase">created</span>
-        </div>
-        <div class="flex space-x-1">
-            <span class="font-bold">{{ $route }}</span>
-            <span class="flex-1 content-repeat-[.] text-gray"></span>
-            <span class="font-bold text-green uppercase">created</span>
-        </div>
+        @isset($controller)
+            <div class="flex space-x-1">
+                <span class="font-bold">{{ $controller }}</span>
+                <span class="flex-1 content-repeat-[.] text-gray"></span>
+                <span class="font-bold text-green uppercase">created</span>
+            </div>
+        @endisset
+
+        @isset($model)
+            <div class="flex space-x-1">
+                <span class="font-bold">{{ $model }}</span>
+                <span class="flex-1 content-repeat-[.] text-gray"></span>
+                <span class="font-bold text-green uppercase">created</span>
+            </div>
+        @endisset
+
+        @isset($migration)
+            <div class="flex space-x-1">
+                <span class="font-bold">{{ $migration }}</span>
+                <span class="flex-1 content-repeat-[.] text-gray"></span>
+                <span class="font-bold text-green uppercase">created</span>
+            </div>
+        @endisset
+
+        @isset($indexRequest)
+            <div class="flex space-x-1">
+                <span class="font-bold">{{ $indexRequest }}</span>
+                <span class="flex-1 content-repeat-[.] text-gray"></span>
+                <span class="font-bold text-green uppercase">created</span>
+            </div>
+        @endisset
+
+        @isset($storeRequest)
+            <div class="flex space-x-1">
+                <span class="font-bold">{{ $storeRequest }}</span>
+                <span class="flex-1 content-repeat-[.] text-gray"></span>
+                <span class="font-bold text-green uppercase">created</span>
+            </div>
+        @endisset
+
+        @isset($updateRequest)
+            <div class="flex space-x-1">
+                <span class="font-bold">{{ $updateRequest }}</span>
+                <span class="flex-1 content-repeat-[.] text-gray"></span>
+                <span class="font-bold text-green uppercase">created</span>
+            </div>
+        @endisset
+
+        @isset($pageIndex)
+            <div class="flex space-x-1">
+                <span class="font-bold">{{ $pageIndex }}</span>
+                <span class="flex-1 content-repeat-[.] text-gray"></span>
+                <span class="font-bold text-green uppercase">created</span>
+            </div>
+        @endisset
+
+        @isset($pageForm)
+            <div class="flex space-x-1">
+                <span class="font-bold">{{ $pageForm }}</span>
+                <span class="flex-1 content-repeat-[.] text-gray"></span>
+                <span class="font-bold text-green uppercase">created</span>
+            </div>
+        @endisset
+
+        @isset($pageCreate)
+            <div class="flex space-x-1">
+                <span class="font-bold">{{ $pageCreate }}</span>
+                <span class="flex-1 content-repeat-[.] text-gray"></span>
+                <span class="font-bold text-green uppercase">created</span>
+            </div>
+        @endisset
+
+        @isset($pageEdit)
+            <div class="flex space-x-1">
+                <span class="font-bold">{{ $pageEdit }}</span>
+                <span class="flex-1 content-repeat-[.] text-gray"></span>
+                <span class="font-bold text-green uppercase">created</span>
+            </div>
+        @endisset
+
+        @isset($pageDelete)
+            <div class="flex space-x-1">
+                <span class="font-bold">{{ $pageDelete }}</span>
+                <span class="flex-1 content-repeat-[.] text-gray"></span>
+                <span class="font-bold text-green uppercase">created</span>
+            </div>
+        @endisset
+
+        @isset($pageDeleteBulk)
+            <div class="flex space-x-1">
+                <span class="font-bold">{{ $pageDeleteBulk }}</span>
+                <span class="flex-1 content-repeat-[.] text-gray"></span>
+                <span class="font-bold text-green uppercase">created</span>
+            </div>
+        @endisset
+
+        @isset($permission)
+            <div class="flex space-x-1">
+                <span class="font-bold">{{ $permission }}</span>
+                <span class="flex-1 content-repeat-[.] text-gray"></span>
+                <span class="font-bold text-green uppercase">created</span>
+            </div>
+        @endisset
+
+        @isset($route)
+            <div class="flex space-x-1">
+                <span class="font-bold">{{ $route }}</span>
+                <span class="flex-1 content-repeat-[.] text-gray"></span>
+                <span class="font-bold text-green uppercase">created</span>
+            </div>
+        @endisset
+
         <span class="text-blue mt-1">Please run 'php artisan migrate'</span>
     </div>
 </div>
